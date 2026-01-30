88120ec671864360eebde00dddcfe1c5f469d59f
Alexandre Mc Lean <alexandre.mclean@gmail.com>
2026-01-29 21:28:48 -0500

    chore: remove unused

--- internal/colony/timeouts.go
+++ internal/colony/timeouts.go
@@ -3,8 +3,6 @@ package colony
 import "time"
 
 const (
-	// realtimeQueryTimeout is for low-latency agent queries (service listing).
-	realtimeQueryTimeout = 500 * time.Millisecond
 
 	// serviceQueryTimeout is for standard service discovery queries.
 	serviceQueryTimeout = 5 * time.Second
